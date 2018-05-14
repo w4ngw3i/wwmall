a7ad60976f91c7c5abc72050485ee52a5c7ecd54
wangwei <china.wangwei@icloud.com>
2018-05-14 10:11:39 +0800

    project init commit

---
+++ src/main/java/com/wwmall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.wwmall.dao;
+
+import com.wwmall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/com/wwmall/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.wwmall.dao;
+
+import com.wwmall.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
